da549d706279a64d6a5d1343936e562ff9656968
Curtis Wilcox <github.com@m.cognize.org>
2019-10-10 02:04:02 -0400

    Change MVP to be more Vue-like

--- p1/README.md
+++ p1/README.md
@@ -4,7 +4,8 @@
 
 ## Outside resources
 - [Fluid Typography](https://css-tricks.com/snippets/css/fluid-typography/)
-
+- [forEach()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
+- [some()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some)
 
 ## Notes for instructor
 *any notes for me to refer to while grading; if none, omit this section*
--- p1/p1.js
+++ p1/p1.js
@@ -3,26 +3,45 @@ var app = new Vue({
 	data: {
 		currentGuesses: 6,
 		currentWord: null,
+		currentWordArray: [],
 		guesses: 6,
 		guessLetter: null,
 		guessedLetters: [],
 		letterDisabled: false,
-		placeholder: null,
+		placeholder: '',
 		words: ['antelope', 'bear', 'cat', 'dolphin', 'eagle', 'ferret', 'giraffe', 'human', 'ibex', 'jellyfish']
+	},
+	computed: {
+		spacedPlaceholder: function () {
+			return this.placeholder.split('').join(' ');
+		}
 	},
 	methods: {
 		checkGuess: function () {
 			checkGuess();
 		},
+		endGame: function () {
+			this.letterDisabled = true;
+			alert('Game over, the word was: ' + this.currentWord);
+		},
 		newGame: function () {
 			newGame();
+		},
+		pickWord: function () {
+			this.currentWord = this.words[Math.floor(Math.random() * this.words.length)];
+			this.currentWordArray = this.currentWord.split('');
+			this.placeholder = '';
+
+			this.currentWordArray.forEach(element => {
+				this.placeholder = this.placeholder + '_';
+			});
 		}
 	},
 	watch: {
 		// end when they've run out of guesses
 		currentGuesses: function (val, oldVal) {
 			if (val <= 0) {
-				endGame();
+				this.endGame();
 			}
 		},
 		// put focus on letter input field after previous guess is cleared
@@ -33,29 +52,32 @@ var app = new Vue({
 		},
 		// end when word has been guessed
 		placeholder: function (val, oldVal) {
-			if (this.currentWord == val.split(' ').join('')) {
-				endGame();
+			if (this.currentWord == val) {
+				this.endGame();
 			}
 		}
+	},
+	mounted: function () {
+		this.pickWord();
 	}
 });
-
-window.addEventListener('load', newGame());
 
 function checkGuess() {
 	// get first entered letter, assumes non-letters are not entered
 	app.guessLetter = app.guessLetter[0].toLowerCase();
-	const guessRegex = RegExp(app.guessLetter, 'g');
 
-	let guessIndexes = [];
+	let letterMatch = false;
 
-	// collect array of indexes for matched letter
-	while ((guessRegex.exec(app.currentWord)) !== null) {
-		guessIndexes.push(guessRegex.lastIndex);
-	}
+	app.currentWordArray.forEach((element, index) => {
+		if (element === app.guessLetter) {
+			letterMatch = true;
+			// replace underscore with correctly guessed letter
+			app.placeholder = app.placeholder.substring(0, index) + element + app.placeholder.substring(index + 1);
+		}
+	});
 
 	// guessed letter not in word
-	if (guessIndexes[0] === undefined) {
+	if (!letterMatch) {
 		const guessedLetterNoMatch = app.guessedLetters.some(element => {
 			return element === app.guessLetter;
 		});
@@ -68,41 +90,14 @@ function checkGuess() {
 			app.guessedLetters = app.guessedLetters.sort();
 		}
 	}
-	else {
-		updatePlaceholder(app.guessLetter, guessIndexes);
-	}
 
 	app.guessLetter = null;
 }
 
-function endGame() {
-	app.letterDisabled = true;
-	alert('Game over, the word was: ' + app.currentWord);
-}
-
 function newGame() {
 	// prepare variables
+	app.pickWord();
 	app.currentGuesses = app.guesses;
-	app.currentWord = app.words[Math.floor(Math.random() * app.words.length)];
 	app.guessedLetters = [];
 	app.letterDisabled = false;
-	app.placeholder = '';
-
-	app.currentWord.split('').forEach(element => {
-		app.placeholder = app.placeholder + '_';
-	});
-
-	app.placeholder = app.placeholder.split('').join(' ');
-}
-
-// replace placeholder character with guessed letter
-function updatePlaceholder(letter, letterIndexes) {
-	// remove spaces
-	let placeAsWord = app.placeholder.split(' ').join('');
-	letterIndexes.forEach(element => {
-		placeAsWord = placeAsWord.substring(0, element - 1) + letter + placeAsWord.substring(element);
-	});
-
-	// re-add spaces
-	app.placeholder = placeAsWord.split('').join(' ');
 }
