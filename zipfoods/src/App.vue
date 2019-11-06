9deca467d6cd5ceafdf1de8d35e25f42452e9c8f
Curtis Wilcox <github.com@m.cognize.org>
2019-11-06 00:04:06 -0500

    Add ZipFoods practice (product img srcs broken)

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" />
+    <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+    <show-featured :category="'snacks'"></show-featured>
+    <show-products></show-products>
+  </div>
+</template>
+
+<script>
+import ShowProducts from "./components/ShowProducts.vue";
+import ShowFeatured from "./components/ShowFeatured.vue";
+
+export default {
+  name: "app",
+  components: { ShowProducts, ShowFeatured }
+};
+</script>
+
+<style lang='scss'>
+@import "./assets/css/zipfoods.scss";
+</style>
---
+++ zipfoods/src/components/ShowProduct.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="product">
+    <div class="product-name">{{ product.name }}</div>
+    <img
+      class="product-thumb"
+      :src="'./../assets/images/products/' + product.id + '.jpg'"
+      :alt="product.name"
+    />
+    <p class="product-description">{{ product.description }}</p>
+    <div class="product-price">${{ product.price }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowProduct",
+  props: ["product"]
+};
+</script>
